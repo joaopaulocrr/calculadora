04f26bd517c55a25f0555eb5312a061e8cc66425
João Paulo <paulocorrentinense@gmail.com>
2023-04-05 02:40:44 -0300

    v.2

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+let resposta = document.querySelector("#res")
+let adicao = document.querySelector("#mais")
+
+
+insert = (num) => {
+      resposta.innerHTML += num
+     
+}
+
+limpar = () => {
+   let minhaString = resposta.toString()
+   
+}
+
+limpartudo = () => {resposta.innerHTML=""}
+
+
